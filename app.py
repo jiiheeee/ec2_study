38ab55ef03993729fcf0c04a74b16e0cd63bbf77
H J <qkrwlgml33@gmail.com>
2023-06-04 21:29:41 +0900

    유저

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return '쓸다리없는거 다 지움ㅋ'
 
+@app.route('/user')
+def user():
+    return '내가 바로 유저다.'
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
